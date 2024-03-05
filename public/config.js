b9c22ded38b53dd6778d771f7060fcf4937f84ec
Yonghua Quan <413067827@qq.com>
2024-03-06 04:34:43 +0800

    Update config.js
    Update subHeader

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__PRE_CONFIG__ = {
   header: 'Server Status',
-  subHeader: 'Servers\' Probes Set up with ServerStatus',
+  subHeader: '小鸡子',
   interval: 1.5,
   footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
 };
